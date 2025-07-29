4cefb09ed0d070cd5af034d8953cb5081a748b86
jittrawanpadsang <s6812022611201@email.kmutnb.ac.th>
2025-07-29 13:50:34 +0700

    0000

---
+++ coding_w05_01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    char name = 'J';
+    int age = 19;
+    float weight = 79.5;
+
+    printf("Student %c is %d yearsold\n", name , age);
+    printf("Her weights is %.1f kg.\n", weight);
+
+    return 0;
+}
